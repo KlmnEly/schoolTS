b0afb32744f8abde3ccd15bc5712f7ce613c85c5
Kelmin Ely <kelminemh@hotmail.com>
2025-09-15 22:55:29 -0500

    refactor (schema.sql): creation of composite key (student_id, schedule_id) in grades and (teacher_course_id, subject_id, day, hour_start, hour_end) in schedules

--- schema.sql
+++ schema.sql
@@ -60,82 +60,108 @@ create table if not exists subjects (
 
 create table if not exists users (
 	id_user serial primary key,
-	role_id int not null references roles(id_role),
+	role_id int not null,
 	username varchar(50) not null unique,
 	password varchar(255) not null,
 	status boolean default TRUE,
 	created_at timestamp default current_timestamp,
-	updated_at timestamp default current_timestamp
+	updated_at timestamp default current_timestamp,
+
+	foreign key (role_id) references roles(id_role)
 );
 
 create table if not exists students (
 	id_student serial primary key,
-	document_type_id int not null references document_types(id_document_type),
+	document_type_id int not null ,
 	full_name varchar(150) not null,
 	document_number varchar (20) not null unique,
 	birth_date date not null,
 	status boolean default true,
 	created_at timestamp default current_timestamp,
-	updated_at timestamp default current_timestamp
+	updated_at timestamp default current_timestamp,
+
+	foreign key (document_type_id) references document_types(id_document_type)
 );
 
 create table if not exists teachers (
 	id_teacher serial primary key,
-	user_id int references users(id_user),
-	document_type_id int references document_types(id_document_type),
+	user_id int not null,
+	document_type_id int not null,
 	full_name varchar(150) not null,
 	document_number varchar (20) not null unique,
 	birth_date date not null,
 	status boolean default true,
 	created_at timestamp default current_timestamp,
-	updated_at timestamp default current_timestamp
+	updated_at timestamp default current_timestamp,
+
+	foreign key (user_id) references users(id_user),
+	foreign key (document_type_id) references document_types(id_document_type)
 );
 
 create table if not exists teacher_profession (
 	id_teacher_profession serial primary key,
-	teacher_id int references teachers(id_teacher),
-	profession_id int references professions(id_profession),
+	teacher_id int not null,
+	profession_id int not null,
 	status boolean default true,
 	created_at timestamp default current_timestamp,
-	updated_at timestamp default current_timestamp
+	updated_at timestamp default current_timestamp,
+
+	foreign key (teacher_id) references teachers(id_teacher),
+	foreign key (profession_id) references professions(id_profession)
 );
 
 create table if not exists teacher_course (
 	id_teacher_course serial primary key,
-	teacher_id int references teachers(id_teacher),
-	course_id int references courses(id_course),
+	teacher_id int not null,
+	course_id int not null,
 	status boolean default true,
 	created_at timestamp default current_timestamp,
-	updated_at timestamp default current_timestamp
+	updated_at timestamp default current_timestamp,
+
+	foreign key (teacher_id) references teachers(id_teacher),
+	foreign key (course_id) references courses(id_course)
 );
 
 create table if not exists schedules (
 	id_schedule serial primary key,
-	teacher_course_id int not null references teacher_course(id_teacher_course),
-	subject_id int references subjects(id_subject),
+	teacher_course_id int not null,
+	subject_id int not null,
 	day day_week not null,
 	hour_start time not null,
 	hour_end time not null,
 	status boolean default TRUE,
 	created_at timestamp default current_timestamp,
-	updated_at timestamp default current_timestamp
+	updated_at timestamp default current_timestamp,
+
+	unique (teacher_course_id, subject_id, day, hour_start, hour_end),
+
+	foreign key (teacher_course_id) references teacher_course(id_teacher_course),
+    foreign key (subject_id) references subjects(id_subject)
 );
 
 create table if not exists student_schedule (
 	id_student_schedule serial primary key,
-	student_id int references students(id_student),
-	schedule_id int references schedules(id_schedule),
+	student_id int not null,
+	schedule_id int not null,
 	status boolean default TRUE,
 	created_at timestamp default current_timestamp,
-	updated_at timestamp default current_timestamp
+	updated_at timestamp default current_timestamp,
+
+	foreign key (student_id) references students(id_student),
+    foreign key (schedule_id) references schedules(id_schedule)
 );
 
 create table if not exists grades (
 	id_grade serial primary key,
-	student_id int not null references students(id_student),
-	schedule_id int not null references schedules(id_schedule),
+	student_id int not null,
+	schedule_id int not null,
 	grade decimal(4,2) not null check (grade between 0.00 and 5.00),
 	status boolean default TRUE,
 	created_at timestamp default current_timestamp,
-	updated_at timestamp default current_timestamp
+	updated_at timestamp default current_timestamp,
+
+	unique (student_id, schedule_id),
+
+	foreign key (student_id) references students(id_student),
+    foreign key (schedule_id) references schedules(id_schedule)
 );
