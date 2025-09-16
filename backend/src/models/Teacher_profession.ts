2eb295722b915acb50648baa0102432d1de57792
Kelmin Ely <kelminemh@hotmail.com>
2025-09-15 22:25:14 -0500

    refactor (/models/Teacher_profession.ts): optional field was changed

--- backend/src/models/Teacher_profession.ts
+++ backend/src/models/Teacher_profession.ts
@@ -3,14 +3,14 @@ import { sequelize } from '../config/database';
 
 export interface TeacherProfessionAttributes {
     id_teacher_profession?: number;
-    teacher_id?: number;
+    teacher_id: number;
     profession_id: number;
     status: boolean;
 }
 
 class TeacherProfession extends Model<TeacherProfessionAttributes> implements TeacherProfessionAttributes {
     public id_teacher_profession!: number;
-    public teacher_id?: number;
+    public teacher_id!: number;
     public profession_id!: number;
     public status!: boolean;
 }
